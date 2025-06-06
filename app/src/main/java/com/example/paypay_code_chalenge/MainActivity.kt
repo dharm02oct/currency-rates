747b76b5a8b6287a8e32af6d2fa501bba5890df8
Dharam <dharm7nov@gmail.com>
2025-06-06 17:21:27 +0530

    comment

--- app/src/main/java/com/example/paypay_code_chalenge/MainActivity.kt
+++ app/src/main/java/com/example/paypay_code_chalenge/MainActivity.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.unit.dp
 import com.example.paypay_code_chalenge.ui.screens.CurrencyScreen
 import com.example.paypay_code_chalenge.ui.viewmodel.Pay2CurrencyViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
+// this is main activity
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   private val currencyViewModel: Pay2CurrencyViewModel by viewModels()
