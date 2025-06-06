9855c261e65c0b4f55a7a20e49d02d512f9b60f4
Dharam <dharm7nov@gmail.com>
2025-06-06 17:40:50 +0530

    branch7

--- app/src/main/java/com/example/paypay_code_chalenge/utils/TimeInHours.kt
+++ app/src/main/java/com/example/paypay_code_chalenge/utils/TimeInHours.kt
@@ -1,5 +1,5 @@
 package com.example.paypay_code_chalenge.utils
-
+//Time
 class TimeInHours(val hours: Int, val minutes: Int, val seconds: Int) {
     override fun toString(): String {
         return String.format("%dh : %02dm : %02ds", hours, minutes, seconds)
